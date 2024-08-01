61fff3925f1244b7aca6b897ab698c467c6005c0
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-08-02 01:38:14 +0200

    1st milestone completed

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 // Calcolatore di prezzo biglietto in base ai dati raccolti dell'utente.
 
 /*
-Recuperare gli elementi d'interesse all'interno del DOM.
+1. Recuperare gli elementi d'interesse all'interno del DOM.
 Inserire le percentuali di sconto
 
-Creazione messaggio
+2. Creazione messaggio
 
 
 1. Chiedere all'utente il numero di chilometri che vuole percorre.
@@ -19,7 +19,45 @@ Inserire i dati raccolti all'interno del DOM.
 */
 
 
+/* Fase di Preparazione */
+
+//1. Recuperare gli elementi d'interesse all'interno del DOM.
+const namefield = document.getElementById('Name');
+const kmsfield = document.getElementById("Kms");
+const agefield = document.getElementById('Age');
+const button = document.getElementById('Create');
+const total = document.getElementById('price')
+
+//2. Creazione messaggio
+let pricemessage = 'Il tuo biglietto costa € '
+let finalprice
+// Tariffa al Km 
+const fare = (0.21)
+
+/* Fase Gestione eventi */
+button.addEventListener('click', function () {
+    const name = namefield.value.trim();
+    const kms = kmsfield.value;
+    const age = agefield.value;
+    console.log(kms, typeof age)
+
+    /* fase dei elaborazione dati */
+
+    let baseprice = (kms * fare)
+
+    finalprice = baseprice
 
 
+    if (age == 'senior') {
+        finalprice *= 0.6;
 
-
+    } else if (age == 'minorenne') {
+        finalprice *= 0.8
+    }
+    else {
+        finalprice = baseprice
+    }
+    console.log(pricemessage + finalprice.toFixed(2))
+    if (finalprice > 0) { total.innerHTML = pricemessage + finalprice.toFixed(2) }
+})
+/*  fase di output */
