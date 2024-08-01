c4e98742813cf2e758358fc9a8d66003c312f608
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-08-01 20:51:50 +0200

    add js steps

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// Calcolatore di prezzo biglietto in base ai dati raccolti dell'utente.
+
+/*
+Recuperare gli elementi d'interesse all'interno del DOM.
+Inserire le percentuali di sconto
+
+Creazione messaggio
+
+
+1. Chiedere all'utente il numero di chilometri che vuole percorre.
+2. Chiedere l'età all'utente.
+3. Calcolare il prezzo del viaggio.
+4a. calcolare lo sconto in base all'età dell'utente.
+4a.  un 20% per i minorenni.
+ab.  un 40% per gli over 60.
+
+Inserire i dati raccolti all'interno del DOM.
+   . Verificare inserito correttamente in console e nel DOM.
+*/
+
+
+
+
+
+
